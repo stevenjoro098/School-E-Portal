5883d16671663e19e579e7a01ba055f86b7eceae
steve <stevenjoro098@gmail.com>
2025-09-18 12:02:25 +0300

    Student detail Functionality upgrade

--- Students/views.py
+++ Students/views.py
@@ -23,7 +23,6 @@ class StudentDetailView(DetailView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-
         context['students_assessments'] = self.object.student_assessments.all()
         return context
 

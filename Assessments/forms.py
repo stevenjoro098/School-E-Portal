d45a30300bd50f2d179cb0efeaa4f27a801b1e82
steve <stevenjoro098@gmail.com>
2025-10-02 10:58:10 +0300

    reverteed

--- Assessments/forms.py
+++ Assessments/forms.py
@@ -12,7 +12,7 @@ class QuestionForm(forms.ModelForm):
 ChoiceFormSet = inlineformset_factory(
     Question,
     Choice,
-    fields=['text','image','is_correct'],
+    fields=['text','is_correct'],
     extra=4,  # Number of choice inputs shown
     can_delete=True
 )

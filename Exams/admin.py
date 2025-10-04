23afeb2c1b1672d5314d76f7da5ec3e5f773dc17
steve <stevenjoro098@gmail.com>
2025-10-04 18:03:25 +0300

    Update the Exam Admin Functionality

--- Exams/admin.py
+++ Exams/admin.py
@@ -9,12 +9,25 @@ class ExamAdmin(admin.ModelAdmin):
     list_filter = ['grade','created']
 
 
-# @admin.register(Subject)
-# class ExamSubjectAdmin(admin.ModelAdmin):
-#     list_display = ['exam','subject']
-#     list_filter = ['exam']
+class SubjectByGradeFilter(admin.SimpleListFilter):
+    title = "Subject"
+    parameter_name = "subject"
+
+    def lookups(self, request, model_admin):
+        exam_id = request.GET.get("exam__id__exact")
+        if exam_id:
+            exam = Exam.objects.filter(id=exam_id).first()
+            if exam and exam.grade:
+                return [(s.id, s.name) for s in exam.grade.grade_subjects.all()]
+        # fallback: show all subjects
+        return [(s.id, s.name) for s in Subject.objects.all()]
+
+    def queryset(self, request, queryset):
+        if self.value():
+            return queryset.filter(subject_id=self.value())
+        return queryset
 
 @admin.register(StudentPerformance)
-class StudentPerformance(admin.ModelAdmin):
-    list_display = ['student', 'exam','subject','performance', 'created']
-    list_filter = ['exam','subject']
+class StudentPerformanceAdmin(admin.ModelAdmin):
+    list_display = ['student', 'exam', 'subject', 'performance', 'created']
+    list_filter = ['exam', SubjectByGradeFilter]

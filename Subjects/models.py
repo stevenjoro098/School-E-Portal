f1c04d371abc3b3656d906c856bb722a16872d53
steve <stevenjoro098@gmail.com>
2025-10-07 12:19:51 +0300

    update

--- Subjects/models.py
+++ Subjects/models.py
@@ -10,8 +10,12 @@ class Grade(models.Model):
         return f"{self.name}"
 
 class Subject(models.Model):
+    numbering = models.PositiveIntegerField(blank=True)
     grade = models.ForeignKey(Grade, on_delete=models.CASCADE, related_name='grade_subjects',blank=True)
     name = models.CharField(max_length=250)
+
+    class Meta:
+        ordering = ('numbering',)
 
     def __str__(self):
         return f"{self.name}"

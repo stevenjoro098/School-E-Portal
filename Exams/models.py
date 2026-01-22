db7db8090279cf3b8e9f348b021cb6fb0b1498d7
stevenjoro098 <stevenjoro098@gmail.com>
2026-01-22 14:58:13 +0300

    update on delete in the exam

--- Exams/models.py
+++ Exams/models.py
@@ -13,23 +13,24 @@ class Exam(models.Model):
     exam_name = models.CharField(max_length=250)
     term = models.CharField(max_length=250, choices=terms)
     created = models.DateField(auto_now_add=True)
-    grade = models.ForeignKey(Grade, on_delete=models.CASCADE, related_name='grade_exam')
+    grade = models.ForeignKey(Grade, on_delete=models.SET_NULL, null=True, blank=True,related_name='grade_exam')
 
     def __str__(self):
         return f"{self.exam_name} - {self.grade}"
 
 class ExamSubject(models.Model):
-    exam = models.ForeignKey(Exam, related_name='exam_subjects', on_delete=models.CASCADE)
-    grade = models.ForeignKey(Grade, on_delete=models.CASCADE, related_name='exam_grade_subjects', blank=True, null=True)
+    exam = models.ForeignKey(Exam, related_name='exam_subjects', on_delete=models.SET_NULL, null=True, blank=True)
+    grade = models.ForeignKey(Grade, on_delete=models.CASCADE, related_name='exam_grade_subjects', blank=True)
     exam_subject = models.CharField(max_length=200)
 
     def __str__(self):
         return f"{self.exam_subject} - {self.exam}"
 
 class StudentPerformance(models.Model):
-    student = models.ForeignKey(Student, related_name='student_performance', on_delete=models.CASCADE)
+    student = models.ForeignKey(Student, related_name='student_performance', on_delete=models.SET_NULL, null=True, blank=True)
     exam = models.ForeignKey(Exam, on_delete=models.CASCADE, related_name='exam_performance')
-    subject = models.ForeignKey(Subject, on_delete=models.CASCADE)
+    student_name = models.CharField(max_length=200)
+    subject = models.ForeignKey(Subject, on_delete=models.SET_NULL, null=True, blank=True)
     performance = models.PositiveIntegerField()
     created = models.DateField(auto_now_add=True)
 
@@ -38,3 +39,8 @@ class StudentPerformance(models.Model):
 
     def __str__(self):
         return f"{self.student} - {self.exam.exam_name} - {self.subject}: {self.performance}"
+
+    def save(self, *args, **kwargs):
+        if self.student and not self.student_name:
+            self.student_name = self.student.full_name
+        super().save(*args, **kwargs)

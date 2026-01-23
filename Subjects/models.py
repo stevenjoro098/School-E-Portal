ce5b457506fd777f31c476023b2cee136aa51678
stevenjoro098 <stevenjoro098@gmail.com>
2026-01-23 14:24:37 +0300

    class teacher functionality added

--- Subjects/models.py
+++ Subjects/models.py
@@ -17,8 +17,8 @@ class Grade(models.Model):
 class Subject(models.Model):
     numbering = models.PositiveIntegerField(blank=True)
     name = models.CharField(max_length=250)
-    grade = models.ForeignKey('Subjects.Grade', on_delete=models.CASCADE, related_name='grade_subjects', blank=True)
-    teacher = models.ForeignKey('Students.Teachers', on_delete=models.CASCADE, related_name='teacher_subjects', blank=True, null=True)
+    grade = models.ForeignKey('Subjects.Grade', on_delete=models.SET_NULL, related_name='grade_subjects', blank=True, null=True)
+    teacher = models.ForeignKey('Students.Teachers', on_delete=models.SET_NULL, related_name='teacher_subjects', blank=True, null=True)
 
     class Meta:
         ordering = ('numbering',)

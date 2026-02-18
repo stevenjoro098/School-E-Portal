f4964dd396702900356a209d4ef7fb6d6e60bb27
stevenjoro098 <stevenjoro098@gmail.com>
2026-02-18 21:50:44 +0300

    update

--- API/views.py
+++ API/views.py
@@ -5,6 +5,7 @@ from Students.models import Student
 from Subjects.models import Subject
 # Create your views here.
 
+# ========================== STUDENTS ====================================
 class StudentListCreateApiView(generics.ListCreateAPIView):
     queryset = Student.objects.all()
     serializer_class = StudentListSerializer
@@ -13,6 +14,11 @@ class StudentDetailApiView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Student.objects.all()
     serializer_class = StudentListSerializer
 
+# ================= Subject ==================================================
 class SubjectListCreate(generics.ListCreateAPIView):
+    queryset = Subject.objects.all()
+    serializer_class = SubjectsSerializer
+
+class SubjectDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Subject.objects.all()
     serializer_class = SubjectsSerializer

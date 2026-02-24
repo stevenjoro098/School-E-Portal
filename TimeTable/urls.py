e2b1806d91168e1ae83f593a9e99d36d69a64431
stevenjoro098 <stevenjoro098@gmail.com>
2026-02-24 22:52:08 +0300

    update

--- TimeTable/urls.py
+++ TimeTable/urls.py
@@ -2,8 +2,6 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('', views.TimeTable.as_view(), name="timetable"),
-# urls.py
     path("api/timetable/<int:grade_id>/", views.GradeTimetableAPIView.as_view(), name="grade-timetable"),
-
     path('edit', views.timetable_matrix_view, name='edit')
 ]

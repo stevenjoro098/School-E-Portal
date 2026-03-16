7220adec2d8886078a1488329000c5ddaf1fc715
stevenjoro098 <stevenjoro098@gmail.com>
2026-03-16 09:53:56 +0300

    update

--- CBC_Elearning/settings.py
+++ CBC_Elearning/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -27,7 +27,26 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
 
+    "handlers": {
+        "file": {
+            "level": "ERROR",
+            "class": "logging.FileHandler",
+            "filename": os.path.join(BASE_DIR, "django_errors.log"),
+        },
+    },
+
+    "loggers": {
+        "django": {
+            "handlers": ["file"],
+            "level": "ERROR",
+            "propagate": True,
+        },
+    },
+}
 # Application definition
 
 INSTALLED_APPS = [
--- Exams/templatetags/dict_extras.py
+++ Exams/templatetags/dict_extras.py
@@ -6,3 +6,10 @@ def get_item(dictionary, key):
     if isinstance(dictionary, dict):
         return dictionary.get(key)
     return None
+
+@register.filter
+def index(sequence, position):
+    try:
+        return sequence[position]
+    except:
+        return None

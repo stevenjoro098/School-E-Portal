caf2108e1e9fda20e3a0949ac7552a7c4319b4c2
stevenjoro098 <stevenjoro098@gmail.com>
2026-04-10 12:13:09 +0300

    update library issued admin

--- Library/admin.py
+++ Library/admin.py
@@ -1,8 +1,12 @@
 from django.contrib import admin
-from .models import Book
+from .models import Book, IssuedBooks
 
 # Register your models here.
 @admin.register(Book)
 class BookAdmin(admin.ModelAdmin):
     list_display = ['title','category','grade','available']
     list_filter = ['grade','category','available']
+
+@admin.register(IssuedBooks)
+class IssuedBooksAdmin(admin.ModelAdmin):
+    list_display = ['book','issued_to','date_issued','return_date']

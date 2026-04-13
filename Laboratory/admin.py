eac1c1d9583b1a50675fabba3353764b683c42bb
stevenjoro098 <stevenjoro098@gmail.com>
2026-04-13 13:13:41 +0300

    update

--- Laboratory/admin.py
+++ Laboratory/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from .models import LabEquipment
 # Register your models here.
+@admin.register(LabEquipment)
+class LabAdmin(admin.ModelAdmin):
+    list_display = ['name','available_quantity','date_added']
